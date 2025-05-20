25fde36b5496647c6e4f9436569ca92d5e704c6e
joneshshrestha <contact@joneshshrestha.com>
2025-05-20 09:49:08 -0500

    new project MCP

---
+++ Markov Chain Implementation/hw_03_code/main.py
@@ -0,0 +1,44 @@
+
+import sys
+import math
+
+# NOTE THAT THESE TRY EXCEPTS ARE ONLY ADDED SO THAT YOU KNOW
+# THAT YOU MUST INSTALL THESE LIBRARIES IF YOU DON"T ALREADY HAVE THEM
+
+try:
+    import gymnasium as gym
+except:
+    print("The gymnasium library is not installed!")
+    print("Please install gymnasium in your python environment using:")
+    print("\tpip install gymnasium")
+    sys.exit(1)
+
+try:
+    import numpy as np
+except:
+    print("The numpy library is not installed!")
+    print("Please install numpy in your python environment using:")
+    print("\tpip install numpy")
+    sys.exit(1)
+
+
+def part_one():
+    # TODO: your code here ...
+    pass
+
+
+def part_two():
+    # TODO: your code here ...
+    pass
+
+
+def main():
+    # TODO: feel free to change this as required
+    # TODO: also, check tutorial.py for some hints on how to implement your experiments
+    part_one()
+    part_two()
+
+
+if __name__ == "__main__":
+    main()
+
