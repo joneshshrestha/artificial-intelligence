38dc11f05c40b723819119992dde4b7cdce83d29
joneshshrestha <contact@joneshshrestha.com>
2026-01-24 23:32:15 -0600

    update

--- Q-Learning/Agent.py
+++ Q-Learning/Agent.py
@@ -1,103 +1,135 @@
 """
+Jonesh Shrestha (2222011)
+Artificial Intelligence II (Winter 2026)
 CSC 580 HW#2 "Agent.py" -- Class Agent, which performs Temporal Difference (TD) Q-Learning.
-
 """
+
 import random
 import numpy as np
+import csv  # for writing and reading the q-table
+
 
 class Agent:
-    """ 
+    """
     An AI agent which controls the snake's movements.
     """
+
     def __init__(self, env, params):
         self.env = env
         self.action_space = env.action_space  # 4 actions for SnakeGame
-        self.state_space = env.state_space    # 12 features for SnakeGame
-        self.gamma = params['gamma']
-        self.alpha = params['alpha']
-        self.epsilon = params['epsilon'] 
-        self.epsilon_min = params['epsilon_min'] 
-        self.epsilon_decay = params['epsilon_decay']
-        ## TO-DO: Choose your data structure to hold the Q table and initialize it
-        self.Q = None
-        
+        self.state_space = env.state_space  # 12 features for SnakeGame
+        self.gamma = params["gamma"]
+        self.alpha = params["alpha"]
+        self.epsilon = params["epsilon"]
+        self.epsilon_min = params["epsilon_min"]
+        self.epsilon_decay = params["epsilon_decay"]
+        # dictionary data structure to hold the Q table and initialize it
+        self.Q = {}
 
     @staticmethod
     def state_to_int(state_list):
-        """ Map state as a list of binary digits, e.g. [0,1,0,0,1,1,1] to an integer."""
+        """Map state as a list of binary digits, e.g. [0,1,0,0,1,1,1] to an integer."""
         return int("".join(str(x) for x in state_list), 2)
-    
+
     @staticmethod
     def state_to_str(state_list):
-        """ Map state as a list of binary digits, e.g. [0,1,0,0,1,1,1], to a string e.g. '0100111'. """
+        """Map state as a list of binary digits, e.g. [0,1,0,0,1,1,1], to a string e.g. '0100111'."""
         return "".join(str(x) for x in state_list)
 
     @staticmethod
     def binstr_to_int(state_str):
-        """ Map a state binary string, e.g. '0100111', to an integer."""
+        """Map a state binary string, e.g. '0100111', to an integer."""
         return int(state_str, 2)
 
-    # (A) 
+    # (A)
     def init_state(self, state):
-        """ Initialize the state's entry in state_table and Q, if anything needed at all."""
-        #
-        #
-        pass # for now
-        
+        """Initialize the state's entry in state_table and Q, if anything needed at all."""
+        s = self.state_to_str(state)
+        if s not in self.Q:
+            self.Q[s] = np.zeros(self.action_space)
+
     # (A)
     def select_action(self, state):
         """
         Do the epsilon-greedy action selection. Note: 'state' is an original list of binary digits.
         It should call the function select_greedy() for the greedy case.
         """
-        #
-        #
-        return np.random.choice(self.action_space) # for now
+        s = self.state_to_str(state)
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(self.action_space)
+        else:
+            return self.select_greedy(state)
 
     # (A)
     def select_greedy(self, state):
-        """ 
-        Greedy choice of action based on the Q-table. 
         """
-        #
-        #
-        return np.random.choice(self.action_space) # for now
-    
+        Greedy choice of action based on the Q-table.
+        """
+        self.init_state(state)
+        s = self.state_to_str(state)
+
+        q_values = self.Q[s]
+        max_q_value = np.max(q_values)
+        max_q_actions = np.where(q_values == max_q_value)[0]
+        return np.random.choice(max_q_actions)
+
     # (A)
     def update_Qtable(self, state, action, reward, next_state):
         """
         Update the Q-table (and anything else necessary) after an action is taken.
         Note that both 'state' and 'next_state' are an original list of binary digits.
         """
-        #
-        #
+        # initialize the current and next states
+        self.init_state(state)
+        self.init_state(next_state)
+
+        # convert the states to integers
+        s = self.state_to_str(state)
+        s_next = self.state_to_str(next_state)
+
+        # get the q-value of the current state and the max q-value of the next state
+        current_q_value = self.Q[s][action]
+        next_max_q_value = np.max(self.Q[s_next])
+        # calculate the target q-value
+        target_q_value = reward + self.gamma * next_max_q_value
+
+        # update the q-value of the current state and action using the Q-learning formula
+        self.Q[s][action] = current_q_value + self.alpha * (
+            target_q_value - current_q_value
+        )
 
         # update the epsilon at the end
         self.adjust_epsilon()
 
     # (A)
     def num_states_visited(self):
-        """ Returns the number of unique states visited. Obtain from the Q table."""
-        #
-        #
-        return 0 # for now
-    
+        """Returns the number of unique states visited. Obtain from the Q table."""
+        return len(self.Q)
+
     # (A)
     def write_qtable(self, filepath):
-        """ Write the content of the Q-table to an output file. """
-        #
-        #
-        pass # for now
+        """Write the content of the Q-table to an output file."""
+        with open(filepath, "w", newline="") as f:
+            writer = csv.writer(f)
+            for s in sorted(self.Q.keys()):
+                for a in range(self.action_space):
+                    writer.writerow([s, a, self.Q[s][a]])
 
     # (A)
     def read_qtable(self, filepath):
-        """ Read in the Q table saved in a csv file. """
-        #
-        #
-        pass # for now
-
+        """Read in the Q table saved in a csv file."""
+        self.Q = {}
+        with open(filepath, "r", newline="") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                s = row[0]
+                a = int(row[1])
+                q = float(row[2])
+                if s not in self.Q:
+                    self.Q[s] = np.zeros(self.action_space)
+                self.Q[s][a] = q
 
     def adjust_epsilon(self):
-        """ Implements the epsilon decay. """
+        """Implements the epsilon decay."""
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
--- Q-Learning/QLearning.py
+++ Q-Learning/QLearning.py
@@ -1,7 +1,9 @@
 """
+Jonesh Shrestha (2222011)
+Artificial Intelligence II (Winter 2026)
 CSC 580 HW#2 "QLearning.py" -- Q-learning for the Snake Game
-
 """
+
 import matplotlib
 import SnakeEnv as snake_env
 import Agent as agent_class
@@ -11,42 +13,43 @@ import matplotlib.pyplot as plt
 import random
 import copy
 
+
 def q_learning(agent, env, max_steps, train=True):
     """
     This function simulates a RL game, where the agent learns the (hopefully) optimal policy
     by Q-learning.  The parameters 'agent' and 'env' are created in the calling function and
-    passed in, while 'max_step' specifies the maximum timesteps to play (Note: continuous 
+    passed in, while 'max_step' specifies the maximum timesteps to play (Note: continuous
     after failing) and 'train' indicates the run is a training or otherwise (i.e., evaluation).
-    Most lines are basic and general, calling functions in the environment or the agent.  
+    Most lines are basic and general, calling functions in the environment or the agent.
     Details depend on the implementations of those components (and their functions).
     """
     # First reset the environment
     state = env.reset()
-    agent.init_state(state) #(A)
-    
+    agent.init_state(state)  # (A)
+
     # Initialize some housekeeping variables
     total_return, n_apples, n_stops, n_goodsteps = 0.0, 0, 0, 0
     done = False
-   
+
     # Play continuously until max_steps.
     for i in range(max_steps):
-        
-        # Select the action to take at this state. 
+
+        # Select the action to take at this state.
         if train:
-            action = agent.select_action(state)  #(A) epsilon greedy selection
+            action = agent.select_action(state)  # (A) epsilon greedy selection
         else:
-            action = agent.select_greedy(state)  #(A) greedy selection
-        
+            action = agent.select_greedy(state)  # (A) greedy selection
+
         # Environment executes the selected action.
-        next_state, reward, done, _ = env.step(action) 
-        
+        next_state, reward, done, _ = env.step(action)
+
         # Q-learning if training -- update the Q-table
         if train:
-            agent.update_Qtable(state, action, reward, next_state)  #(A) 
-            
+            agent.update_Qtable(state, action, reward, next_state)  # (A)
+
         # Update to prepare for the next iteration
         state = next_state
-        
+
         # Accumulate the total return and other counts from this step
         total_return += pow(agent.gamma, i) * reward
 
@@ -59,18 +62,22 @@ def q_learning(agent, env, max_steps, train=True):
         elif reward == -100:  # i.e., done
             n_stops += 1
         #
-    return total_return, n_apples, n_stops, n_goodsteps, agent.num_states_visited() #(A)
-    
-    
-# Do q_learning for 'num_runs' times.  For each run, 'num_steps' steps is done.
-def run_ql(max_runs, max_steps, in_params, qtable_file, display = False, train = False):
-    """
+    return (
+        total_return,
+        n_apples,
+        n_stops,
+        n_goodsteps,
+        agent.num_states_visited(),
+    )  # (A)
 
-    """
+
+# Do q_learning for 'num_runs' times.  For each run, 'num_steps' steps is done.
+def run_ql(max_runs, max_steps, in_params, qtable_file, display=False, train=False):
+    """ """
     num_runs = max_runs
     num_steps = max_steps
     results_list = []
-    best_return = float('-inf')
+    best_return = float("-inf")
     best_qtable = None
 
     for run in range(num_runs):
@@ -87,12 +94,17 @@ def run_ql(max_runs, max_steps, in_params, qtable_file, display = False, train =
         if not train and qtable_file is not None:
             agent.read_qtable(qtable_file)
 
-        ret = q_learning(agent, env, num_steps, train=train) # training=False for evaluation
+        ret = q_learning(
+            agent, env, num_steps, train=train
+        )  # training=False for evaluation
         results_list.append(ret)
 
-        env.close() # for each run
-        print ("* Run {}: Return={:>8.3f}, #Apples={}, #Stops={}, #GoodSteps={}, #UniqueStatesVisited={}"
-               .format(run, ret[0], ret[1], ret[2], ret[3], ret[4]))
+        env.close()  # for each run
+        print(
+            "* Run {}: Return={:>8.3f}, #Apples={}, #Stops={}, #GoodSteps={}, #UniqueStatesVisited={}".format(
+                run, ret[0], ret[1], ret[2], ret[3], ret[4]
+            )
+        )
 
         if train:
             if ret[0] > best_return:
@@ -101,30 +113,35 @@ def run_ql(max_runs, max_steps, in_params, qtable_file, display = False, train =
 
     if train:
         agent.Q = best_qtable  # overwrite the agent's last Q table
-        agent.write_qtable(qtable_file) # so that this function can be used
+        agent.write_qtable(qtable_file)  # so that this function can be used
 
     return results_list
 
+
 ##===================================================
 ## Call run_ql() for either/both training and evaluation
-num_runs = 1      #10
-num_steps = 1000 #300   #1000
+num_runs = 1  # 10
+num_steps = 1000  # 300 #1000
 
 params = dict()
-params['gamma'] = 0.95
-params['alpha'] = 0.7
-params['epsilon'] = 0.6  # exploration probability at start
-params['epsilon_min'] = .01  # minimum epsilon
-params['epsilon_decay'] = .995  # exponential decay rate for epsilon
+params["gamma"] = 0.95
+params["alpha"] = 0.7
+params["epsilon"] = 0.6  # exploration probability at start
+params["epsilon_min"] = 0.01  # minimum epsilon
+params["epsilon_decay"] = 0.995  # exponential decay rate for epsilon
 
-qtable_file = "qtable_2025.csv" #"qtable_true.csv" #None
+qtable_file = "qtable_2025.csv"  # "qtable_true.csv" #None
 
 # Call run_ql() for either training or evaluation
-results_list = run_ql(num_runs, num_steps, params, qtable_file, display = True, train = False) # evaluation
-#results_list = run_ql(num_runs, num_steps, params, qtable_file, display = False, train = True) # training
+# results_list = run_ql(num_runs, num_steps, params, qtable_file, display = True, train = False) # evaluation
+results_list = run_ql(
+    num_runs, num_steps, params, qtable_file, display=False, train=True
+)  # training
 
 results = np.array(results_list)
 cmean = np.mean(results, axis=0)
-print ("\n** Mean: Return={:>8.3f}, #Apples={}, #Stops={}, #GoodSteps={}, #UniqueStatesVisited={}"
-           .format(cmean[0], cmean[1], cmean[2], cmean[3], cmean[4]))
-           
+print(
+    "\n** Mean: Return={:>8.3f}, #Apples={}, #Stops={}, #GoodSteps={}, #UniqueStatesVisited={}".format(
+        cmean[0], cmean[1], cmean[2], cmean[3], cmean[4]
+    )
+)
